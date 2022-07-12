b567ef70946ee4a861a445697d8a74dc844cb72f
Uri Baynesay <uri552@gmail.com>
2022-07-12 11:44:48 +0300

    markerter intial setup

--- src/pages/MarkerterApp.jsx
+++ src/pages/MarkerterApp.jsx
@@ -5,12 +5,18 @@ import { MarkerterForm } from "../cmp/markerter-form"
 export const MarkerterApp = () => {
   const [formsCount, setFormsCount] = useState(0)
 
-  const onSubmit = (form) => {}
+  const onSubmit = (form) => {
+    console.log(form)
+  }
 
   return (
     <div className="markerter-app-container">
+      
       <FormsInfo formsCount={formsCount} />
-      <MarkerterForm onSubmit={onSubmit} />
+
+      <section className="markerter-form">
+        <MarkerterForm onSubmit={onSubmit} />
+      </section>
     </div>
   )
 }
