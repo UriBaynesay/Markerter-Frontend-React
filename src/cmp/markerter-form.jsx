e37ca4705e819026453c0236b1d229461d80b617
Uri Baynesay <uri552@gmail.com>
2022-07-12 10:40:13 +0300

    initial setup cmps and state

---
+++ src/cmp/markerter-form.jsx
@@ -0,0 +1,5 @@
+export const MarkerterForm = ({onSubmit})=>{
+    return <form className="markerter-form-container">
+        <h1>hello from the form</h1>
+    </form>
+}
--- src/pages/MarkerterApp.jsx
+++ src/pages/MarkerterApp.jsx
@@ -1,5 +1,16 @@
-export const MarkerterApp=()=>{
-    return <div className="markerter-app-container">
-        <h1>hello form MarkerterApp</h1>
+import { useState } from "react"
+import { FormsInfo } from "../cmp/forms-info"
+import { MarkerterForm } from "../cmp/markerter-form"
+
+export const MarkerterApp = () => {
+  const [formsCount, setFormsCount] = useState(0)
+
+  const onSubmit = (form) => {}
+
+  return (
+    <div className="markerter-app-container">
+      <FormsInfo formsCount={formsCount} />
+      <MarkerterForm onSubmit={onSubmit} />
     </div>
+  )
 }
